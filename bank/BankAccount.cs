0939f2a277bcf2cc8abc1ced57be1320cb5669ea
blekhol <peiszr@gmail.com>
2025-04-03 18:13:51 +0200

    bank account

---
+++ bank/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    internal abstract class BankAccount : BankingService
+    {
+        private double balance;
+
+        public BankAccount(Owner owner) : base(owner)
+        {
+
+        }
+
+        public double Balance { get => balance; }
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public abstract bool Withdraw(double amount);
+    }
+}
---
+++ bank/BankingService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    internal abstract class BankingService
+    {
+        private Owner owner;
+
+        protected BankingService(Owner owner)
+        {
+            this.owner = owner;
+        }
+
+        internal Owner Owner { get => owner; }
+    }
+}
